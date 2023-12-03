a57ed4e14a75ad3ef207521d35feadd38d111f83
Kağan Değirmen <kagandegirmen@gmail.com>
2023-12-03 21:31:40 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./globals.css";
+
+type Props = {};
+
+const App = (props: Props) => {
+  return <h1 className="text-3xl font-bold underline">Hello Mementogram!</h1>;
+};
+
+export default App;
