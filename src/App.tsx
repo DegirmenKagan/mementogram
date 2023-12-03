d883d81de1707ce9dfe7d032ba50e736dc31e7ef
Kağan Değirmen <kagandegirmen@gmail.com>
2023-12-03 21:59:07 +0300

    Inıtıail Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
-
+import { Home } from "./_root/pages";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 type Props = {};
 
 const App = (props: Props) => {
-  return <h1 className="text-3xl font-bold underline">Hello Mementogram!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-in" element={<SignUpForm />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
