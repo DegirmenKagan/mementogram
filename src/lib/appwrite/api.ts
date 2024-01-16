523d2b22af4277cc83632534d0cffa66f0bbae08
Kağan Değirmen <kagandegirmen@gmail.com>
2024-01-17 00:22:07 +0300

    api.ts > saveUser to database

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account,appwriteConfig,avatars, databases } from "./config";
 export async function createUserAccount(user: INewUser) {
   try {
     const newAccount = await account.create(
@@ -9,9 +9,36 @@ export async function createUserAccount(user: INewUser) {
       user.password,
       user.name
     );
-    return newAccount;
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+    const newUser = await saveUserToDatabase(
+      {
+        accountId:newAccount.$id,
+        email:newAccount.email,
+        name:newAccount.name,
+        username:user.username,
+        imageUrl: avatarUrl,
+      });  
+
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
   }
+}
+
+export async function saveUserToDatabase(user:{accountId: string;email:string;name:string;imageUrl: URL;username?:string}){
+try {
+  const newUser = await databases.createDocument(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    ID.unique(),
+    user,
+  )
+  return newUser;
+} catch (error) {
+  console.log(error);
+}
 }
